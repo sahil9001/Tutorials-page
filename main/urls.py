0450f942f0b529ebafe2d2cc66492683a612f914
Sahil Silare <sahilsilare@gmail.com>
2020-03-25 21:40:51 +0530

    Login/Logout User

--- main/urls.py
+++ main/urls.py
@@ -19,4 +19,7 @@ from . import views
 app_name = "main"
 urlpatterns = [
     path("", views.homepage,name="homepage"),
+    path("register/",views.register,name="register"),
+    path("logout/",views.logout_request,name="logout"),
+    path("login/",views.login_request,name="login")
 ]
