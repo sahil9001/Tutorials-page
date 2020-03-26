d3c0bb5b650547251e25975913db5740e1d47a28
Sahil Silare <sahilsilare@gmail.com>
2020-03-26 22:12:20 +0530

    Changes to be committed: 	modified:   db.sqlite3 	modified:   main/admin.py 	new file:   main/forms.py 	new file:   main/migrations/0003_auto_20200326_1620.py 	new file:   main/migrations/0004_auto_20200326_1622.py 	new file:   main/migrations/0005_auto_20200326_1635.py 	modified:   main/models.py 	modified:   main/views.py

--- main/views.py
+++ main/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Tutorial
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
+from .forms import NewUserForm
 # Create your views here.
 def homepage(request):
     return render(request=request,template_name="main/home.html",context={"tutorials":Tutorial.objects.all})
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = NewUserForm(request.POST)
         if form.is_valid():
             user = form.save()
             username = form.cleaned_data.get('username')
@@ -21,7 +22,7 @@ def register(request):
             for msg in form.error_messages:
                 messages.error(request, f"{msg}: {form.error_messages[msg]}")
     
-    form = UserCreationForm
+    form = NewUserForm
     return render(request,"main/register.html",context={"form":form})
 
 def logout_request(request):
